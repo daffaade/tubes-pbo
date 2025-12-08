21e01571ee198d934d2008d31f040bbbca846a87
apriliaalfa <alfagusasti@gmail.com>
2025-12-08 14:01:35 +0700

    mainfinal

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,16 +5,7 @@ import ui.LoginFrame;
 
 public class MainApp {
     public static void main(String[] args) {
-        // Tambahkan di Main.java, sebelum MainFrame dibuat
-        try {
-            // Menggunakan tampilan sistem operasi (misalnya Windows, macOS)
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            
-            // ATAU: Menggunakan L&F yang lebih modern (seperti FlatLaf, tapi butuh library eksternal)
-            // Jika Anda tidak ingin library eksternal, System L&F adalah pilihan terbaik.
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
         
         SwingUtilities.invokeLater(new Runnable() {
             @Override
